6c5382950683b73d9bd8dd996233dd234d57be4c
Electronical-ARAI-san <61216147+H-J-Ainashi@users.noreply.github.com>
2020-11-09 11:13:45 +0900

    Initialization

---
+++ BlanksTestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlanksTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
